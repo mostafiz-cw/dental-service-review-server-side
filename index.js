cc1d9c6315ba213e7a7beb5a432f3eb15e4bc545
mostafiz <rafi11zxy@gmail.com>
2022-11-12 04:54:36 +0600

    vercel congfig and json update

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run() {
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = addService.find(query);
-      const services = await cursor.toArray();
+      const services = await cursor.limit(3).toArray();
       res.send(services);
     });
 
